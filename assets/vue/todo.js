8adc8f85001db67afa38db688279c510f1052a5e
Faris Widyantho <radenfaris@gmail.com>
2017-11-25 17:56:12 +0700

    feature done undone

--- assets/vue/todo.js
+++ assets/vue/todo.js
@@ -14,35 +14,48 @@ Vue.component('my-todo', {
             
           </div>
           <div class="col-md-2 col-sm-2 right">
-            <button v-if="t.status == 'true'" type="button" class="btn btn-success btn-sm">Done</button>
-            <!-- <button type="button" class="btn btn-warning">Warning</button> -->
-            <button v-else type="button" class="btn btn-danger btn-sm">Not done</button>
+            <button v-if="t.status == true" type="button" class="btn btn-success btn-sm" @click="undone(t._id)">Done</button>
+            <button v-else type="button" class="btn btn-danger btn-sm" @click="done(t._id)">Not done</button>
           </div>
         </div>
       </div>
       <div class="card-footer">
-        <a href="#" class="badge badge-pill badge-warning">Edit Todo</a>
-        <a href="#" class="badge badge-pill badge-danger">Delete Todo</a>
+        <a href="#" class="badge badge-pill badge-warning" >Edit Todo</a>
+        <a href="#" class="badge badge-pill badge-danger" >Delete Todo</a>
       </div>
     </div>
   </div>
   `,
-  
+  props: ['todos','username'],
   data: function () {
     return {
-      todos: [],
-      username: ''
+      
     }
   },
   methods: {
-    getTodo: function(){
-      axios.get('http://localhost:3000/api/mytodo',{
+    done: function(id){
+      axios.put('http://localhost:3000/api/done/'+id,[],{
         'headers': {'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhMTA1Mzg2NDExZjNlN2Q4ZGMxNjNhYiIsImVtYWlsIjoicmFkZW5mYXJpc0BnbWFpbC5jb20iLCJpYXQiOjE1MTEwMTkzOTh9._3mopJCs703DWdPGq_kxYuW2Qd98r4zB6XJBLYFaJoY'}
       })
       .then((response)=>{
-        console.log(response)
-        this.username = response.data.user.email
-        this.todos = response.data.todos
+        var pos = this.todos.findIndex(function(e){
+          return e._id == response.data.before._id;
+        })
+        this.todos[pos].status = true
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+    },
+    undone: function(id){
+      axios.put('http://localhost:3000/api/undone/'+id,[],{
+        'headers': {'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhMTA1Mzg2NDExZjNlN2Q4ZGMxNjNhYiIsImVtYWlsIjoicmFkZW5mYXJpc0BnbWFpbC5jb20iLCJpYXQiOjE1MTEwMTkzOTh9._3mopJCs703DWdPGq_kxYuW2Qd98r4zB6XJBLYFaJoY'}
+      })
+      .then((response)=>{
+        var pos = this.todos.findIndex(function(e){
+          return e._id == response.data.before._id;
+        })
+        this.todos[pos].status = false
       })
       .catch((error)=>{
         console.log(error);
@@ -50,7 +63,6 @@ Vue.component('my-todo', {
     }
   },
   created(){
-    this.getTodo()
-    console.log('here')
+    // this.getTodo()
   }
 })
